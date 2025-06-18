dd6ef0deb7dae3214ac4ffc9222b63bdb6ef1bc1
Thanaian <thanaian@example.com>
2025-06-18 10:53:11 +0300

    Fix: added i18n support for Arabic and English

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'ar',
+    localeDetection: true,
+  },
   images: {
     formats: ['image/webp'],
     minimumCacheTTL: 60 * 60 * 24, // 1 يوم

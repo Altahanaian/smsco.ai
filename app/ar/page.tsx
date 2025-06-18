79fc4b3eae804e62d8eb18da5018147569eef817
Thanaian <thanaian@example.com>
2025-06-18 11:02:43 +0300

    Fix: removed all locale page.tsx files causing redirect loop

--- app/ar/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = () => {
-  return <div>مرحبًا بك في SMSCO</div>;
-};
-
-export default Page;
--- app/en/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = () => {
-  return <div>Welcome To SMSCO</div>;
-};
-
-export default Page;
